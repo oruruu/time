b9e64b4b758ff03e902ab91397a870258ec3dcde
oruruu <101560806+oruruu@users.noreply.github.com>
2022-03-14 13:47:43 +0300

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,17 @@
+function randomBackgroundImage() {
+   
+    // массив картинок
+    let images    = ['img/1.jpg', 'img/2.jpg', 'img/3.jpg'];
+   
+    // контейнер у которого будем менять фоновое изображение
+    let imageBox  = document.body.style.backgroundImage;
+   
+    // выбираем случайную картинку
+    let image     = Math.floor( Math.random() * images.length );
+   
+    // меняем фон на эту картинку
+    document.body.style.backgroundImage = 'url(' + images[image] + ')';
+}
+
+// вызываем при загрузке/перезагрузке страницы
+window.onload = randomBackgroundImage;
---
+++ main.js
@@ -0,0 +1,60 @@
+let final = 0
+let hour = 0
+let hourMin = 0
+let min = 0
+let d = 0
+let hourstest = [ 24, 25, 26, 27];
+document.querySelector('.btn').addEventListener('click', () => {
+    let date = document.querySelector('.inp').value;
+    let date2 = document.querySelector('.inp2').value;
+
+    // let gg =Number(date.substring(0, 2));
+    let g = Number(date.substring(0, 2));
+    let g2 =Number(date.substring(3, 5));
+    let g3 = (g*60) + g2
+
+
+
+    let q =Number(date2.substring(0, 2));
+    let q2 =Number(date2.substring(3, 5));
+    let q3 = (q*60) + q2
+
+    if (q <g) {
+        q=hourstest[q]
+        let q3 = (q*60) + q2
+        let final = q3 - g3
+        d = d+final
+            if (d >= 60) {
+                hour = Math.trunc(d/60);
+                hourMin = hour*60
+                min = d-hourMin
+            }
+            else {
+                min = d
+            }
+    }
+    else {
+    let final = q3 - g3
+    d = d+final
+        if (d >= 60) {
+            hour = Math.trunc(d/60);
+            hourMin = hour*60
+            min = d-hourMin
+        }
+        else {
+            min = d
+        }
+}
+    document.querySelector('.res').innerHTML =('ты отработал ' + hour +' часов ' + min + ' минут');
+
+})
+document.querySelector('.btn2').addEventListener('click', () => {
+    final = 0
+    hour = 0
+    hourMin = 0
+    min = 0
+    d = 0
+    document.querySelector('.res').innerHTML =('ты отработал ' + hour +' часов ' + min + ' минут');
+})
+
+
